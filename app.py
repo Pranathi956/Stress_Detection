37fc7692b5e10df33e4a53037b0874fdc3b534a5
Pranathi956 <pranathiummalaneni@gmail.com>
2026-05-25 22:48:59 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,6 +13,12 @@ from groq import Groq
 import warnings
 warnings.filterwarnings('ignore')
 
+import streamlit as st
+from groq import Groq
+
+# 👇 Add here
+client = Groq(api_key=st.secrets["GROQ_API_KEY"])
+
 # ── Page config — must be first Streamlit call ────────────────────
 st.set_page_config(
     page_title="StressLens",
